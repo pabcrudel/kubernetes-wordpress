3cb494fd9d53e1466903584735f1c0ef7dd80b97
pabcrudel <pabcrudel@gmail.com>
2024-02-17 17:28:46 +0100

    chore: Reorganize project structure

--- kubernetes/scripts/delete.sh
+++ kubernetes/scripts/delete.sh
@@ -2,22 +2,22 @@
 
 echo
 echo Deleting Wordpress
-kubectl delete -f wordpress-deployment.yml
+kubectl delete -f ../wordpress-deployment.yml
 echo Done
 
 echo
 echo Deleting MySQL
-kubectl delete -f mysql-deployment.yml
+kubectl delete -f ../mysql-deployment.yml
 echo Done
 
 echo
 echo Deleting CluesterIP
-kubectl delete -f cluster-ip.yml
+kubectl delete -f ../cluster-ip.yml
 echo Done
 
 echo
 echo Deleting Load Balancer
-kubectl delete -f load-balancer.yml
+kubectl delete -f ../load-balancer.yml
 echo Done
 
 echo
--- kubernetes/scripts/deploy.sh
+++ kubernetes/scripts/deploy.sh
@@ -2,22 +2,22 @@
 
 echo
 echo Deploying Wordpress
-kubectl apply -f wordpress-deployment.yml
+kubectl apply -f ../wordpress-deployment.yml
 echo Done
 
 echo
 echo Deploying MySQL
-kubectl apply -f mysql-deployment.yml
+kubectl apply -f ../mysql-deployment.yml
 echo Done
 
 echo
 echo Deploying CluesterIP
-kubectl apply -f cluster-ip.yml
+kubectl apply -f ../cluster-ip.yml
 echo Done
 
 echo
 echo Deploying Load Balancer
-kubectl apply -f load-balancer.yml
+kubectl apply -f ../load-balancer.yml
 echo Done
 
 wp_lb_port=$(kubectl get svc wordpress-loadbalancer \
